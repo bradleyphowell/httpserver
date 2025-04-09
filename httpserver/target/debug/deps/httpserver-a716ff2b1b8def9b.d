836943f7e0afb6b916331e8f8dc0cb066ab1d565
bradleyphowell <bradleyphowell@gmail.com>
2025-04-09 22:14:02 +0100

    Initial commit

---
+++ httpserver/target/debug/deps/httpserver-a716ff2b1b8def9b.d
@@ -0,0 +1,5 @@
+C:\Users\bradl\Programming\rust\httpserver\httpserver\target\debug\deps\libhttpserver-a716ff2b1b8def9b.rmeta: src\main.rs
+
+C:\Users\bradl\Programming\rust\httpserver\httpserver\target\debug\deps\httpserver-a716ff2b1b8def9b.d: src\main.rs
+
+src\main.rs:
---
+++ httpserver/target/debug/deps/httpserver-edf522b1ed58429e.d
@@ -0,0 +1,5 @@
+C:\Users\bradl\Programming\rust\httpserver\httpserver\target\debug\deps\libhttpserver-edf522b1ed58429e.rmeta: src\main.rs
+
+C:\Users\bradl\Programming\rust\httpserver\httpserver\target\debug\deps\httpserver-edf522b1ed58429e.d: src\main.rs
+
+src\main.rs:
